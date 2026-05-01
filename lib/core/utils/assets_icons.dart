8806ed1db657c2304ebc0a4c8ba77cc42bb16245
عبدالرحمن محمد زيدان محمد <abdallrahmanzidan@fsc.bu.edu.eg>
2026-05-01 16:20:47 +0300

    add  folder utils - contain icons and images assets; define constants for asset paths in app_colors, assets_icons, and assets_image

---
+++ lib/core/utils/assets_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color backgroundColor = Color(0xff0F1014);
+  static const Color barColor = Color(0xff242A32);
+  static const Color textColor = Color(0xffffffff);
+  
+}
