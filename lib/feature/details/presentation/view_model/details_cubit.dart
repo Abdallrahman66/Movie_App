11be0d29d618ca71f2e3e906a40583f51fbeb0d4
BadrSayed2 <BadrSayed2@example.com>
2026-05-05 04:09:18 +0300

    feat: implement details and similar data fetching with error handling

--- lib/feature/details/presentation/view_model/details_cubit.dart
+++ lib/feature/details/presentation/view_model/details_cubit.dart
@@ -1,8 +1,37 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:movie_app/core/network/result_api.dart';
+import 'package:movie_app/feature/details/domain/entities/details_entity.dart';
+import 'package:movie_app/feature/details/domain/entities/similar_entity.dart';
+import 'package:movie_app/feature/details/domain/use_case/get_details_use_case.dart';
+import 'package:movie_app/feature/details/domain/use_case/get_similar_use_case.dart';
 
 part 'details_state.dart';
 
 class DetailsCubit extends Cubit<DetailsState> {
-  DetailsCubit() : super(DetailsInitial());
+  DetailsCubit(this._details, this._similar) : super(DetailsInitial());
+  final GetDetailsUseCase _details;
+  final GetSimilarUseCase _similar;
+
+  Future<void> getDetails() async {
+    emit(DetailsLoadingState());
+    final result = await _details.invoke();
+    switch (result) {
+      case SuccessApi<DetailsEntity>():
+        emit(DetailsSuccessState());
+      case ErrorApi<DetailsEntity>():
+        emit(DetailsErrorState(result.errorMassage));
+    }
+  }
+
+  Future<void> getSimilar() async {
+    emit(SimilarLoadingState());
+    final result = await _similar.invoke();
+    switch (result) {
+      case SuccessApi<SimilarEntity>():
+        emit(SimilarSuccessState());
+      case ErrorApi<SimilarEntity>():
+        emit(SimilarErrorState(result.errorMassage));
+    }
+  }
 }
--- lib/feature/details/presentation/view_model/details_state.dart
+++ lib/feature/details/presentation/view_model/details_state.dart
@@ -4,3 +4,21 @@ part of 'details_cubit.dart';
 abstract class DetailsState {}
 
 class DetailsInitial extends DetailsState {}
+
+class DetailsLoadingState extends DetailsState {}
+
+class DetailsSuccessState extends DetailsState {}
+
+class DetailsErrorState extends DetailsState {
+  DetailsErrorState(this.error);
+  final String error;
+}
+
+class SimilarLoadingState extends DetailsState {}
+
+class SimilarSuccessState extends DetailsState {}
+
+class SimilarErrorState extends DetailsState {
+  SimilarErrorState(this.error);
+  final String error;
+}
