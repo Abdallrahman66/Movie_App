6434d952da0abdadf73ee78f9410a6e556d2d39a
عبدالرحمن محمد زيدان محمد <abdallrahmanzidan@fsc.bu.edu.eg>
2026-05-05 23:15:44 +0300

    -feat:Recommended Movie

--- lib/feature/home/presentation/view/home_screen.dart
+++ lib/feature/home/presentation/view/home_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
    static const String routeName = "HomeScreen";
   const HomeScreen({super.key});
- 
-
   @override
   Widget build(context) {
     return Scaffold(
