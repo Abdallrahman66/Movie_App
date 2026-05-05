db859cca8ac67b8883b354411cc44b022addd112
BadrSayed2 <BadrSayed2@example.com>
2026-05-05 05:43:24 +0300

    Refactor code structure for improved readability and maintainability

--- lib/feature/details/presentation/view/screens/details_movie_screen.dart
+++ lib/feature/details/presentation/view/screens/details_movie_screen.dart
@@ -12,12 +12,79 @@ class DetailsMovieScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
       appBar: CustomAppBar(
         title: "Detail",
         actions: [CustomActionBookmark()],
         leading: Image.asset(AssetsIcon.back_button_icon),
       ),
-      body: Scaffold(backgroundColor: AppColor.backgroundColor),
+      body: Column(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 250,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/cover.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              Positioned(
+                bottom: 10,
+                right: 10,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.star_border,
+                        color: Color(0xffFF8700),
+                        size: 18,
+                      ),
+                      Text(" 9.5", style: TextStyle(color: Color(0xffFF8700))),
+                    ],
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 30,
+                top: 170,
+                child: Row(
+                  crossAxisAlignment: .end,
+
+                  children: [
+                    Image.asset(
+                      "assets/images/image.png",
+                      height: 120,
+                      width: 95,
+                    ),
+                    Text(
+                      "Spiderman No Way Home",
+                      textAlign: .left,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: .w600,
+                        color: Color(0xffFFFFFF),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
