9db1d0c53c2a635f94200dee691a10306d582468
عبدالرحمن محمد زيدان محمد <abdallrahmanzidan@fsc.bu.edu.eg>
2026-05-07 14:38:32 +0300

    added loading shimmer for hom screen

--- lib/feature/home/presentation/widget/home_loading_shimmer.dart
+++ lib/feature/home/presentation/widget/home_loading_shimmer.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/widgets/loading_shimmer.widget.dart';
 
-
 class HomeLoadingShimmer extends StatelessWidget {
   const HomeLoadingShimmer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [_title(), _list(), _title(), _list(), _title(), _list()],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _section(),
+          _list(),
+          _section(),
+          _list(),
+          _section(),
+          _list(),
+        ],
+      ),
     );
   }
 
-  SliverToBoxAdapter _title() {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+  Widget _section() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      child: Align(
+        alignment: Alignment.centerLeft,
         child: LoadingShimmer(
           width: 140,
           height: 20,
@@ -25,24 +34,22 @@ class HomeLoadingShimmer extends StatelessWidget {
     );
   }
 
-  SliverToBoxAdapter _list() {
-    return SliverToBoxAdapter(
-      child: SizedBox(
-        height: 260,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(10),
-              child: LoadingShimmer(
-                width: 160,
-                height: 240,
-                borderRadius: BorderRadius.circular(12),
-              ),
-            );
-          },
-        ),
+  Widget _list() {
+    return SizedBox(
+      height: 260,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: LoadingShimmer(
+              width: 160,
+              height: 240,
+              borderRadius: BorderRadius.circular(12),
+            ),
+          );
+        },
       ),
     );
   }
