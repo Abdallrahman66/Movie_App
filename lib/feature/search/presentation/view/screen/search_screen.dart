6806a9111056248340bf630d4ca38e5b79bbf5b1
Waelzahran55 <swael1144@gmail.com>
2026-05-07 12:53:08 +0300

    feat: enhance loading state in SearchScreen with LoadingShimmer widgets

--- lib/feature/search/presentation/view/screen/search_screen.dart
+++ lib/feature/search/presentation/view/screen/search_screen.dart
@@ -5,17 +5,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:movie_app/core/common/app_bar.dart';
 import 'package:movie_app/core/common/search_bar.dart';
+
 import 'package:movie_app/core/dialogs/show_error_ui.dart';
+
 import 'package:movie_app/core/utils/app_colors.dart';
+import 'package:movie_app/core/widgets/loading_shimmer.widget.dart';
 
 import 'package:movie_app/feature/menu_drawer/view/menu_drawer_screen .dart';
 
 import 'package:movie_app/feature/search/data/api/home_search_api.dart';
+
 import 'package:movie_app/feature/search/data/repo/search_repository_imp.dart';
 
 import 'package:movie_app/feature/search/domain/use_case/search_movies_use_case.dart';
 
 import 'package:movie_app/feature/search/presentation/view/screen/invalid_search_screen.dart';
+
 import 'package:movie_app/feature/search/presentation/view/widget/movie_item.dart';
 
 import '../../view_model/cubit/search_cubit.dart';
@@ -31,6 +36,7 @@ class SearchScreen extends StatelessWidget {
       create: (context) => SearchCubit(
         SearchMoviesUseCase(SearchRepositoryImpl(HomeSearchApi())),
       ),
+
       child: const _SearchScreenBody(),
     );
   }
@@ -51,6 +57,7 @@ class _SearchScreenBodyState extends State<_SearchScreenBody> {
   @override
   void dispose() {
     debounce?.cancel();
+
     controller.dispose();
 
     super.dispose();
@@ -102,7 +109,75 @@ class _SearchScreenBodyState extends State<_SearchScreenBody> {
 
                 builder: (context, state) {
                   if (state is SearchLoading) {
-                    return const Center(child: CircularProgressIndicator());
+                    return ListView.builder(
+                      itemCount: 6,
+
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+
+                          child: Row(
+                            children: [
+                              LoadingShimmer(
+                                width: 100,
+
+                                height: 140,
+
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+
+                              const SizedBox(width: 14),
+
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                                  children: [
+                                    LoadingShimmer(
+                                      height: 24,
+
+                                      width: 180,
+
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+
+                                    const SizedBox(height: 18),
+
+                                    LoadingShimmer(
+                                      height: 18,
+
+                                      width: 80,
+
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+
+                                    const SizedBox(height: 14),
+
+                                    LoadingShimmer(
+                                      height: 18,
+
+                                      width: 120,
+
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+
+                                    const SizedBox(height: 14),
+
+                                    LoadingShimmer(
+                                      height: 18,
+
+                                      width: 100,
+
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
                   }
 
                   if (state is SearchEmpty) {
--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,7 @@ import 'package:movie_app/feature/search/presentation/view/screen/Search_screen.
 import 'package:movie_app/feature/search/presentation/view/screen/invalid_search_screen.dart';
 import 'package:movie_app/feature/watch_list/presentation/view/watch_list.dart';
 
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:movie_app/feature/search/data/api/home_search_api.dart';
-import 'package:movie_app/feature/search/data/repo/search_repository_imp.dart';
-
-import 'package:movie_app/feature/search/domain/use_case/search_movies_use_case.dart';
-
-import 'package:movie_app/feature/search/presentation/view_model/cubit/search_cubit.dart';
 
 void main() async {
   runApp(const MyApp());
