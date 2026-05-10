175cba43800afda29da0cf38b753be2b92b5ffd3
Abdallrahman Mohamed Zidan <abdallrahmanzidan@fsc.bu.edu.eg>
2026-05-10 19:11:58 +0300

    Revert "Feature/search/wael zahran"

--- lib/core/utils/assets_image.dart
+++
@@ -1,4 +0,0 @@
-abstract class AssetsImage {
-  static const String NoMovieYet = "assets/images/no_movies_yet.png";
-  static const String NoResultSearch = "assets/images/no_result_search.png";
-}
--- lib/feature/search/domain/repo/search_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:movie_app/core/network/result_api.dart';
-import 'package:movie_app/feature/search/domain/entities/movie_entity.dart';
-
-
-abstract interface class SearchRepository {
-  Future<ResultApi<List<MovieEntity>>> searchMovies(
-    String query,
-  );
-}
